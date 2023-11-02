cc0ebbb61ed0d1db5d133ec064f56fa76f61b78c
Evgen <polivanov004@gmail.com>
2023-11-02 17:17:24 +0300

    add function upload and show image or file

--- routers/utils_router.py
+++ routers/utils_router.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, File, UploadFile
+from fastapi.responses import FileResponse
 import httpx
 from auth import User
 from .auth_router import current_user
 from faker import Faker
+import uuid
+import os
+from random import randint
 
 faker = Faker("ru_RU")
 
@@ -11,7 +15,7 @@ utils_router = APIRouter(
     tags=["Utils"]
 )
 
-
+IMAGEDIR = "images/"
     
 
 @utils_router.get("/test-auth")
@@ -29,3 +33,24 @@ def unprotected_route():
         return f"Hello, {x}"
     except Exception as ex:
         raise HTTPException(status_code=555, detail=str(ex))
+
+@utils_router.post("/upload/")
+async def create_upload_file(file: UploadFile = File(...)):
+    
+    file.filename = f"{uuid.uuid4()}.jpg"
+    contents = await file.read()
+
+    
+    with open(f"{IMAGEDIR}{file.filename}", "wb") as f:
+        f.write(contents)
+
+    return {"filename":file}
+
+@utils_router.get("/show/")
+async def read_random_file():
+    files = os.listdir(IMAGEDIR)
+    random_index = randint(0, len(files) - 1)
+
+    path = f"{IMAGEDIR}{files[random_index]}"
+
+    return FileResponse(path)
